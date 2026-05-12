5626aeac2213809ac4f96a9b9d252652591d734d
bhaskarmalasani <bhaskararao1.m@gmail.com>
2026-05-12 15:39:58 +0530

    updated getCount api response

--- routes/count_routes.js
+++ routes/count_routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { getCountData, getPartnerServiceCount,getHomeCount } = require('../controllers/count_controller');
 const authMiddleware = require('../middleware/auth_middleware');
 const rateLimiter = require('../middleware/rate_middleware');
+const { stripDuplicateRecordsFromGetCountResponse } = require('../middleware/count_middleware');
 router.use(rateLimiter);
 
-router.post('/getCount', rateLimiter, authMiddleware, getCountData);
+router.post('/getCount', authMiddleware, stripDuplicateRecordsFromGetCountResponse, getCountData);
 router.get('/getPartnerServiceCount', rateLimiter, authMiddleware, getPartnerServiceCount);
 router.get('/getHomeCount', rateLimiter, authMiddleware, getHomeCount);
 module.exports = router;

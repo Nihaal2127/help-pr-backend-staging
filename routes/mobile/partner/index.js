530cc2b6ddbb7db246e411184ed6cbed10a4499c
bhaskarmalasani <bhaskararao1.m@gmail.com>
2026-05-21 12:31:00 +0530

    password validations in mobile

--- routes/mobile/partner/index.js
+++ routes/mobile/partner/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../../../controllers/mobile/partner/partner_register_controller');
-const { partnerRegisterMiddleware } = require('../../../middleware/mobile/partner/partner_register_middleware');
+const { register, login } = require('../../../controllers/mobile/partner/partner_controller');
+const {
+  partnerRegisterMiddleware,
+  partnerLoginMiddleware,
+} = require('../../../middleware/mobile/partner/partner_middleware');
 
 router.post('/register', partnerRegisterMiddleware, register);
+router.post('/login', partnerLoginMiddleware, login);
 
 module.exports = router;

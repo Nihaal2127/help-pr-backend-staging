068ff277471bd583096676a0dde1aec1e10cc731
saibhaskar <saibhaskarmail@gmail.com>
2026-06-18 10:58:41 +0530

    updated chat implementation

--- src/modules/chat/services/chat.service.js
+++ src/modules/chat/services/chat.service.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 const Chat = require("../models/chat.model");
+const User = require("../../../../models/user");
+const Dispute = require("../../../../models/dispute");
 const ChatError = require("../utils/chatError");
-const { assertChatAccess, assertChatManageAccess } = require("../utils/chatAccess");
+const { assertChatAccess, assertChatManageAccess, resolveChatFranchiseId } = require("../utils/chatAccess");
 const { fieldLabel } = require("../../../../utils/field_labels");
+const { USER_TYPE_CUSTOMER, USER_TYPE_EMPLOYEE } = require("../../../../constants/user_types");
+const { buildRolesForParticipantIds } = require("./chatProvisioning.service");
+const { createSystemMessage } = require("./message.service");
+const { emitToChat } = require("../sockets/chatEmitter");
+
+const HANDOFF_CHAT_TYPES = new Set(["support", "dispute"]);
 
 const ensureObjectId = (value, fieldName) => {
   if (!mongoose.Types.ObjectId.isValid(value)) {
@@ -89,9 +97,91 @@ const removeParticipant = async (chatId, userId, actorUserId, userType) => {
 };
 
 const transferChat = async (chatId, newAssignedTo, actorUserId, userType) => {
+  ensureObjectId(newAssignedTo, "newAssignedTo");
   const chat = await assertChatManageAccess(chatId, actorUserId, userType);
+  const newAssigneeId = String(newAssignedTo);
+
+  if (String(chat.assignedTo || "") === newAssigneeId) {
+    return chat;
+  }
+
+  if (!HANDOFF_CHAT_TYPES.has(chat.type)) {
+    chat.assignedTo = newAssignedTo;
+    await chat.save();
+    return chat;
+  }
+
+  const newEmployee = await User.findOne({
+    _id: newAssignedTo,
+    type: USER_TYPE_EMPLOYEE,
+    deleted_at: null,
+    is_active: true,
+  })
+    .select("_id name franchise_id chat")
+    .lean();
+
+  if (!newEmployee) {
+    throw new ChatError("New assignee must be an active employee.", 400, "INVALID_ASSIGNEE");
+  }
+
+  if (newEmployee.chat === false) {
+    throw new ChatError("This employee is not available for chat.", 403, "CHAT_FORBIDDEN");
+  }
+
+  const chatFranchiseId = await resolveChatFranchiseId(chat);
+  if (
+    chatFranchiseId &&
+    newEmployee.franchise_id &&
+    String(newEmployee.franchise_id) !== String(chatFranchiseId)
+  ) {
+    throw new ChatError("New assignee must belong to the chat franchise.", 403, "CHAT_FORBIDDEN");
+  }
+
+  const previousAssignedId = chat.assignedTo ? String(chat.assignedTo) : null;
+  const participantUsers = await User.find({ _id: { $in: chat.participants || [] } })
+    .select("_id type name")
+    .lean();
+
+  const customerIds = participantUsers
+    .filter((user) => Number(user.type) === USER_TYPE_CUSTOMER)
+    .map((user) => String(user._id));
+
+  if (!customerIds.length) {
+    throw new ChatError("Support and dispute chats must include a customer participant.", 409, "CHAT_INVALID");
+  }
+
+  const previousEmployee =
+    participantUsers.find((user) => String(user._id) === previousAssignedId) ||
+    participantUsers.find((user) => Number(user.type) === USER_TYPE_EMPLOYEE);
+
+  const nextParticipants = [...new Set([...customerIds, newAssigneeId])];
+  chat.participants = nextParticipants;
+  chat.roles = await buildRolesForParticipantIds(nextParticipants);
   chat.assignedTo = newAssignedTo;
+  chat.isGroup = false;
   await chat.save();
+
+  if (chat.type === "dispute") {
+    const disputeFilter = chat.context?.disputeId
+      ? { _id: chat.context.disputeId, deleted_at: null }
+      : { chat_id: chat._id, deleted_at: null };
+
+    await Dispute.updateOne(disputeFilter, {
+      $set: {
+        employee_id: newAssignedTo,
+        updated_at: new Date(),
+      },
+    });
+  }
+
+  const previousName = previousEmployee?.name || "previous handler";
+  const newName = newEmployee.name || "new handler";
+  const systemMessage = await createSystemMessage(
+    chat._id,
+    `Chat transferred from ${previousName} to ${newName}.`
+  );
+  emitToChat(chat._id, "receive_message", systemMessage);
+
   return chat;
 };
 
